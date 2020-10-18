0c00aa3a8b2de1b0c7f5ada24bfd9bb39b67f56a
Alex Redshaw <alex.redshaw@gmail.com>
2020-10-18 22:49:59 +0100

    Refactor totalSpendingAcrossAllUsers and add better jsdocs

--- Aggregator.mjs
+++ Aggregator.mjs
@@ -5,10 +5,20 @@ export class AggregatableTransactionData {
         this.transactionData = transactionData;
     }
 
+    /**
+     * Returns the collection of transactions ordered by name of user
+     * @return {AggregatableTransactionData} Returns AggregatableTransactionData
+     */
     orderByUserName() {
         return this.transactionData.sort((current, comparitor) => current.user.localeCompare(comparitor.user));
     }
     
+
+    /**
+     * Returns an array of all transactions for a user 
+     * @param {string} user User to query data by
+     * @return {AggregatableTransactionData} Returns AggregatableTransactionData
+     */
     totalTransactionsfor(user) {
         return this.transactionData.filter(transaction => transaction.user === user);
     }
@@ -16,6 +26,7 @@ export class AggregatableTransactionData {
     /**
      * Returns an object describing the spending of a user 
      * @param {string} user User to query data by
+     * @return {SpendingData} Returns SpendingData
      */
     totalSpendingForUser(user) {
         const transactionsForUser = this.totalTransactionsfor(user.toLowerCase());
@@ -42,16 +53,18 @@ export class AggregatableTransactionData {
         );
     }
 
+    /**
+     * Compiles an array of users and their spending on each category
+     * @return {Array<SpendingData>} Returns an array of each users spending
+     */
     totalSpendingAcrossAllUsers() {
-        let spendingTable = [];
-
-        for (const user of this.getListOfUsers()) {
-            spendingTable.push(this.totalSpendingForUser(user));
-        }
-
-        return spendingTable;
+        return this.getListOfUsers().map(user => this.totalSpendingForUser(user));
     }
 
+    /**
+     * Gets a list of all unique users
+     * @return {Array<string>} Returns a list of users
+     */
     getListOfUsers() {
         return Array.from(new Set(this.transactionData.map(row => row.user)));
     }
